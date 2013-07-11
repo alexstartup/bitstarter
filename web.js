28ae6e1341e43295f2787d43f46fa5bdaa48b56d
Aleksandra Sojka <asojka@kpf.com>
2013-07-11 00:53:16 +0000

    updated web.fixed double require

--- web.js
+++ web.js
@@ -15,9 +15,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-var fs = require('fs')
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
